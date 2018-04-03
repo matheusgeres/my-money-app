7c1fa6b4a7389ad8254e55f032217f6c654dd2e6
matheusgeres <matheusgeres@gmail.com>
2018-04-02 23:06:49 -0300

    Finalização do backend para inserir, remover, apagar. Inserção das mensagens traduzidas de error.

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -1,5 +1,5 @@
-const retful = require('node-restful')
-const mongoose = retful.mongoose
+const restful = require('node-restful')
+const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -8,12 +8,12 @@ const creditSchema = new mongoose.Schema({
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
     status: { type: String, required: false, uppercase: true,
         enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
 
-const billingCycleSchema = new moongose.Schema({
+const billingCycleSchema = new mongoose.Schema({
     name: { type: String, required: true },
     month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
